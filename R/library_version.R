372472cc0bc713168ab05328f818dcd740848c1e
Simon Ress <simon-ress@mt-ag.com>
2023-03-21 21:07:11 +0100

    fix loading

--- R/library_version.R
+++ R/library_version.R
@@ -36,42 +36,42 @@ library_version = function(package, version, lib.search.path = .libPaths()[1]){
   #detach other already loaded versions of the package
   suppressWarnings(try(detach(paste0("package:",package), character.only = TRUE, force = T), silent = T))
 
-  #load specified version of the package
-  # package.install.path = paste0(lib.search.path, "/", package, "_", version)
-  # cat("Loading package '", package, "' (version ", version, ") from '", package.install.path, "'", "\n", sep="")
-  # message = try(library(package, lib.loc = package.install.path, character.only = TRUE), silent = TRUE) # character.only = TRUE <- needed when paste0() or object used
+  #Load specified version of the package
 
-  error = TRUE
-  while(error==TRUE) {
-    message = try(library(package, lib.loc = package.install.path, character.only = TRUE), silent = TRUE)
-    preventing.detaching = gsub(paste0(".*ist importiert von ","\\s*(.*?)\\s*"," und kann deshalb nicht entladen werden.*"), "\\1", message)
-    preventing.detaching = regmatches(preventing.detaching, gregexpr("(?<=‘|')\\S+(?=’|')", preventing.detaching, perl = TRUE))[[1]]
-    cat(preventing.detaching, "\n")
-    for(p in preventing.detaching){
-      unloadNamespace(p)
-      #.libPaths(.libPaths()[-grep(p,.libPaths())])
-      #suppressWarnings(try(detach(paste0("package:",p), character.only = TRUE, force = T), silent = T))
+    #Unload all namespaces the packages of interest is imported in, in order to load it
+      #e.g. ggmap_3.0.2 imports ggplot2, therefore loading a new ggplot2 version could cause an error, because the old one can't be unloaded
+
+    cat("Try to load packages from: ", package.install.path, "\n", sep ="")
+    exit = FALSE
+    while(exit==FALSE) {
+      message = try(library(package, lib.loc = package.install.path, character.only = TRUE), silent = TRUE)
+      preventing.detaching = regmatches(message, gregexpr("ist importiert von (.*?) und kann deshalb nicht entladen werden", message, perl = TRUE))[[1]]
+      preventing.detaching = try(regmatches(preventing.detaching, gregexpr("(?<=‘|')\\S+(?=’|')", preventing.detaching, perl = TRUE))[[1]], silent=T)
+      if(!inherits(preventing.detaching, "try-error")) {
+        for(p in preventing.detaching){
+          cat("unloadNamespace: ", p, "\n")
+          unloadNamespace(p)
+          #.libPaths(.libPaths()[-grep(p,.libPaths())])
+          #suppressWarnings(try(detach(paste0("package:",p), character.only = TRUE, force = T), silent = T))
+        }
+      } else exit = TRUE
     }
-  }
 
+    #test if loading worked
+    #if not:
+    if(inherits(message, "try-error")) {
+      cat("ERROR MESSAGE: ", message[1])
+      cat("----")
+      stop(paste0("The requested version of the packages can't be loaded!
+        -> 1. Check the entered package name and version number (e.g. package= 'ggplot2' & version = '3.0.0')
+        -> 2. Check if there is a folder named '",package, "_", version, "' in '", lib.search.path, "' which contains the package-folder '", package, "'."))
+    } else {
+      #if loading worked:
+      #update search path
+      update_packages_search_path(package.install.path)
+    }
 
-
-
-  #test if loading worked
-  #if not:
-  if(inherits(message, "try-error")) {
-    cat("ERROR MESSAGE: ", message[1])
-    cat("----")
-    stop(paste0("The requested version of the packages can't be loaded!
-      -> 1. Check the entered package name and version number (e.g. package= 'ggplot2' & version = '3.0.0')
-      -> 2. Check if there is a folder named '",package, "_", version, "' in '", lib.search.path, "' which contains the package-folder '", package, "'."))
-  } else {
-    #if loading worked:
-    #update search path
-    update_packages_search_path(package.install.path)
-  }
-
-  #test whether desired version is now loaded or not
-  if(version == utils::packageVersion(package)) message(paste0("Check: Correct Version (->", version, ") of packages '", package, "' loaded"))
-  if(version != utils::packageVersion(package)) warning(paste0("Check: Version '", utils::packageVersion(package), "' instead of desired version ", version, " of packages '", package, "' loaded"))
+    #test whether desired version is now loaded or not
+    if(version == utils::packageVersion(package)) message(paste0("Check: Correct Version (->", version, ") of packages '", package, "' loaded"))
+    if(version != utils::packageVersion(package)) warning(paste0("Check: Version '", utils::packageVersion(package), "' instead of desired version ", version, " of packages '", package, "' loaded"))
 }

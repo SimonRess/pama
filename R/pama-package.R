bf9ddac35508be3f3fd948606f1ab4afa454d42d
Simon Ress <simon-ress@mt-ag.com>
2023-05-26 18:04:48 +0200

    -

---
+++ R/pama-package.R
@@ -0,0 +1,11 @@
+#' @description
+#' Find the overview of all pama-functions here: `help(package = "pama")`
+#'
+#' To learn more about pama, start with the vignettes: `browseVignettes("pama")`
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL

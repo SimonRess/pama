bddf431e2e6fbec3d41fcc4713767a9dae6b0dfd
Simon Ress <simon-ress@mt-ag.com>
2023-07-04 00:41:11 +0200

    First deployment version: 0.1.0 - adding early var-type checks - adjusting param description

--- R/get_requirements.R
+++ R/get_requirements.R
@@ -3,8 +3,8 @@
 #'
 #' @rdname get_requirements
 #'
-#' @param file.name (chr vector): Name of the requirement-file
-#' @param path (chr vector): Folder of the requirement-file
+#' @param req.file.name (chr vector): Name of the requirement-file
+#' @param req.file.path (chr vector): Folder of the requirement-file
 #'
 #' @details Returns the information of the requirements-file
 #'
@@ -23,13 +23,24 @@
 #'
 #' @author Simon Ress
 
-get_requirements = function(file.name="requirements.txt", path=getwd()) {
+get_requirements = function(req.file.name="requirements.txt",
+                            req.file.path=getwd()) {
   # Reads the requirements-file and outputs a list of packages to install/load
-  # :param file.name (chr vector): Name of the requirements-file (-> USE .txt-file !!!)
-  # :param path (chr vector): Path to the requirements-file
+  # :param req.file.name (chr vector): Name of the requirements-file (-> USE .txt-file !!!)
+  # :param req.file.path (chr vector): Path to the requirements-file
   # :return: List of list including vectors with packages to install/load: <package_name> <version>
   # :side-effects: none
-  req = scan(paste0(path,"/",file.name), what="", sep="\n", quiet = T)
+
+  #Check format of args
+    if(!is.character(req.file.name)) {stop(paste0("'req.file.path = ", req.file.name, "' is not of type <character>. Please provide a character value!"))}
+    if(!length(req.file.name)==1) {stop(paste0("'req.file.path = ", req.file.name, "' is not a single character value. Please provide a single character value!"))}
+
+    if(!is.character(req.file.path)) {stop(paste0("'req.file.path = ", req.file.path, "' is not of type <character>. Please provide a character value!"))}
+    if(!length(req.file.path)==1) {stop(paste0("'req.file.path = ", req.file.path, "' is not a single character value. Please provide a single character value!"))}
+
+
+
+  req = scan(paste0(req.file.path,"/",req.file.name), what="", sep="\n", quiet = T)
   r.version = sub("R-version ", "",req[grep("R-version", req)])
   out = list(r.version = r.version)
   packages = req[grep("Packages:", req)+1:length(req)]
@@ -39,7 +50,6 @@ get_requirements = function(file.name="requirements.txt", path=getwd()) {
   if(length(lists)==0) { #Take all packages
     out[["main"]] = packages
   } else { # assign packages to lists
-    #out = list(main = packages[1:grep("#", packages)[1]-1])
     out[["main"]] = packages[1:grep("#", packages)[1]-1]
     out[["main"]] = ifelse(identical(character(0),out[["main"]]), NA, out[["main"]])
     for(i in grep("#", packages)){

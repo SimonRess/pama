2a48f6711874859bb032f55af54d3aad8daaf9a0
Simon Ress <simon-ress@mt-ag.com>
2023-07-04 00:42:45 +0200

    First deployment version: 0.1.0 - adding early var-type checks - adjusting param description
    - adjusting manual

--- R/load_requirements.R
+++ R/load_requirements.R
@@ -8,11 +8,11 @@
 #'
 #' @rdname load_requirements
 #'
-#' @param req.file.path (chr vector): Name of the requirements-file
-#' @param req.file.name (chr vector): Folder of the requirements-file
+#' @param req.file.path (chr value): Name of the requirements-file
+#' @param req.file.name (chr value): Folder of the requirements-file
 #' @param lists (chr vector): Names of lists (without '#' !) specified in the requirements-file to use
-#' @param library.folder.path (chr vector): Folder in which the subfolder "lib" should be created
-#' @param library.folder.name (chr vector): Name of the lib-folder
+#' @param library.folder.path (chr value): Path in which the folder is located from which the packages are to be loaded
+#' @param library.folder.name (chr value): Name of the lib-folder
 #'
 #' @details Wrapper around get_requirements() and library_version(). Loading/Attatching all packages from the requirements-file.
 #'
@@ -47,7 +47,11 @@
 #' @author Simon Ress
 
 
-load_requirements = function(req.file.path=getwd(), req.file.name="requirements.txt", lists="all",  library.folder.path=getwd(), library.folder.name="lib") {
+load_requirements = function(req.file.path=getwd(),
+                             req.file.name="requirements.txt",
+                             lists="all",
+                             library.folder.path=getwd(),
+                             library.folder.name="lib") {
   # Reads the requirements-file and outputs a list of packages to install/load
   # :param req.file.name (chr vector): Path to the requirements-file
   # :param req.file.path (chr vector): Name of the requirements-file (-> USE .txt-file !!!)
@@ -57,12 +61,30 @@ load_requirements = function(req.file.path=getwd(), req.file.name="requirements.
   # :return: ???
   # :side-effects: Loading all selected package-lists from the requirements-file
 
+
+ #Check format of args
+  if(!is.character(req.file.path)) {stop(paste0("'req.file.path = ", req.file.path, "' is not of type <character>. Please provide a character value!"))}
+  if(!length(req.file.path)==1) {stop(paste0("'req.file.path = ", req.file.path, "' is not a single character value. Please provide a single character value!"))}
+
+  if(!is.character(req.file.name)) {stop(paste0("'req.file.name = ", req.file.name, "' is not of type <character>. Please provide a character value!"))}
+  if(!length(req.file.name)==1) {stop(paste0("'req.file.name = ", req.file.name, "' is not a single character value. Please provide a single character value!"))}
+
+  if(!is.character(lists)) {stop(paste0("'lists = ", lists, "' is not of type <character>. Please provide a character vector!"))}
+
+  if(!is.character(library.folder.path)) {stop(paste0("'library.folder.path = ", library.folder.path, "' is not of type <character>. Please provide a character value!"))}
+  if(!length(library.folder.path)==1) {stop(paste0("'library.folder.path = ", library.folder.path, "' is not a single character value. Please provide a single character value!"))}
+
+  if(!is.character(library.folder.name)) {stop(paste0("'library.folder.name = ", library.folder.name, "' is not of type <character>. Please provide a character value!"))}
+  if(!length(library.folder.name)==1) {stop(paste0("'library.folder.name = ", library.folder.name, "' is not a single character value. Please provide a single character value!"))}
+
+
+
   #detaching all other (non base) packages, before loading packages from requirements-file
     detach_none_base()
 
 
   #get content of requirements-file
-  req = get_requirements(file.name=req.file.name, path=req.file.path)
+  req = get_requirements(req.file.name=req.file.name, req.file.path=req.file.path)
 
   #update package search paths
   lib = paste(library.folder.path, library.folder.name, sep="/")
